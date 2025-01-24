56cc0137e466d75a5466af9442c90f1886024cf1
Batuhan <batuhanozkan.cont@gmail.com>
2025-01-24 17:22:26 +0300

    initial commit

---
+++ API/Program.cs
@@ -0,0 +1,40 @@
+
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace API
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            //ServiceRegistration.ServiceRegistrationBusiness(builder.Services, builder.Configuration.GetConnectionString("SqlConnectionString"));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Core/Repositories/Concrete/BaseRepositoryNoTracking.cs
@@ -0,0 +1,32 @@
+﻿using Core.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Shared.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories.Concrete
+{
+    public class BaseRepositoryNoTracking<T, TContext> : BaseRepository<T, TContext>, IBaseRepository<T>
+        where T : BaseEntity
+        where TContext : DbContext
+    {
+        private TContext _context;
+        public BaseRepositoryNoTracking(TContext context) : base(context)
+        {
+            _context = context;
+        }
+        private DbSet<T> Table => _context.Set<T>();
+        public IQueryable<T> GetAllAsNoTrackingWithIdentityResolution()
+        {
+            return Table.AsNoTrackingWithIdentityResolution();
+        }
+        public IQueryable<T> GetWhereAsNoTrackingWithIdentityResolution(Expression<Func<T, bool>> method)
+        {
+            return Table.Where(method).AsNoTrackingWithIdentityResolution();
+        }
+    }
+}
